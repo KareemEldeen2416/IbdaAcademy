7f6115ef00f30cbeabadffd2e697f30c4a995d33
KareemEldeen Ahmed <kareemeldeenahmed@gmail.com>
2024-11-22 03:11:59 +0200

    TEST

--- JS/app.js
+++ JS/app.js
@@ -15,3 +15,38 @@ nav.setAttribute("style",expand);
 expanded = true;
 }
 });
+
+
+let arrowButton = document.getElementsByClassName("detailButton");
+let coursesDetails = document.getElementsByClassName("courseDetails");
+console.log(coursesDetails);
+console.log(arrowButton);
+let expandDetails = "height: 200px; transition-duration:1s;"
+let shrinkDetails = "height: 0px; transition-duration:1s;"
+let rotateIcon = "transform: rotateZ(-180deg); transition-duration: 1s; filter:drop-shadow(0 0 1px purple);"
+let reverseRotateIcon = "transform: rotateZ(0deg); transition-duration: 1s;"
+let detailExpanded = false;
+
+
+for(let i = 0 ; i < arrowButton.length ;i++){
+arrowButton[i].addEventListener("click",function(){
+if(detailExpanded){
+arrowButton[i].setAttribute("style",reverseRotateIcon);
+coursesDetails[i].setAttribute("style",shrinkDetails);
+detailExpanded=false;
+}else{
+arrowButton[i].setAttribute("style",rotateIcon);
+coursesDetails[i].setAttribute("style",expandDetails);
+detailExpanded=true;
+}
+});
+}
+
+
+
+
+
+
+
+
+

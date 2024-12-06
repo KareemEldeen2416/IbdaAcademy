45a57c342976b56cc8bda20a9092d3a4f916fcc7
KareemEldeen Ahmed <kareemeldeenahmed@gmail.com>
2024-12-06 10:23:04 +0200

    Another update

---
+++ JS/tracks.js
@@ -0,0 +1,18 @@
+//Burger Animation
+burgerButton = document.getElementById("burger");
+nav = document.getElementById("nav");
+expand = "animation-name:expand;animation-duration:1s;visibility:visible;"+
+"animation-fill-mode:forwards;";
+shrink = "animation-name:shrink;animation-duration:1s;visibility:hidden;"+
+"animation-fill-mode:forwards;";
+console.log(burgerButton);
+expanded = false;
+burgerButton.addEventListener("click",function(){
+if(expanded){
+nav.setAttribute("style",shrink);
+expanded = false;
+}else{
+nav.setAttribute("style",expand);
+expanded = true;
+}
+});
